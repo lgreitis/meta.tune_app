f9eff27c474dcc1c54322b0b7a75e209702f9781
LGreitis <lukas.greicius@ktu.edu>
2021-03-06 21:31:11 +0200

    simple login logic

--- App.js
+++ App.js
@@ -1,18 +1,74 @@
+import * as React from 'react';
+import { AsyncStorage, Button, Text, TextInput, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
-import LoginStack from './routes/loginStack'
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginStack from './routes/loginStack';
 import HomeStack from './routes/homeStack'
+import loginUtils from './lib/loginUtils'
 
-export default function App() 
-{
+export const AuthContext = React.createContext();
+
+const Stack = createStackNavigator();
+
+export default function App({ navigation }) {
+  const [state, dispatch] = React.useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+          return {
+            ...prevState,
+            userLoginInfo: action.loginInfo,
+            isLoading: false,
+          };
+        case 'SIGN_IN':
+          return {
+            ...prevState,
+            isSignout: false,
+            userLoginInfo: action.loginInfo,
+          };
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isSignout: true,
+            userLoginInfo: null,
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userLoginInfo: null,
+    }
+  );
+
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async data => {
+        loginUtils.submitHandler(data.email, data.password, (res) => {
+          if (res !== false){
+            dispatch({ type: 'SIGN_IN', loginInfo: res });
+          }
+        })
+      },
+      signOut: () => dispatch({ type: 'SIGN_OUT' }),
+    }),
+    []
+  );
+
   return (
-
+    <AuthContext.Provider value={authContext}>
       <NavigationContainer>
-        {!false
-          ? <LoginStack />
-          : <HomeStack />
-        }
+        <Stack.Navigator>
+          {state.userLoginInfo == null ? (
+            <Stack.Screen
+              name="LoginStack"
+              component={LoginStack}
+            />
+          ) : (
+            <Stack.Screen name="HomeStack" component={HomeStack} />
+          )}
+        </Stack.Navigator>
       </NavigationContainer>
-
+    </AuthContext.Provider>
   );
 }
