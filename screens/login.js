b55480e19f5f91d368580cd8bdb1f5ae3c1369a6
LGreitis <lukas.greicius@ktu.edu>
2021-03-07 16:26:10 +0200

    fix a big mess

--- screens/login.js
+++ screens/login.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { StyleSheet, View, Text, TextInput, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { StyleSheet, View, Text, TextInput, TouchableWithoutFeedback, Keyboard, Image, TouchableOpacity } from 'react-native';
 import { AuthContext } from "../App.js"
 import Button from '../components/button';
 
@@ -8,70 +8,6 @@ export default function Login() {
   const [password, setPassword] = useState('');
 
   const { signIn } = React.useContext(AuthContext);
-
-  /*
-  const changeHandlerUsername = (val) => {
-    setUsername(val);
-  }
-
-  const changeHandlerPassword = (val) => {
-    setPassword(val);
-  }
-  
-
-  const getRooms = () => {
-    fetch('http://88.119.36.191:8888/api/rooms',
-      {
-        method: 'GET'
-
-      }).then(response => {
-        if (response.status === 200) {
-          return response.json();
-        } else {
-          throw new Error('Something went wrong on api server!');
-        }
-      })
-      .then(response => {
-        console.log(response);
-        // ...
-      }).catch(error => {
-        console.log(error);
-      });
-  }
-  */
-
-      let formBody = [];
-      for (let property in details) {
-        let encodedKey = encodeURIComponent(property);
-        let encodedValue = encodeURIComponent(details[property]);
-        formBody.push(encodedKey + "=" + encodedValue);
-      }
-      formBody = formBody.join("&");
-
-    fetch('http://88.119.36.191:8888/api/login',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: formBody
-
-
-      }).then(response => {
-        if (response.status === 200) {
-          return response.json();
-        } else {
-          throw new Error('Something went wrong on api server!');
-        }
-      })
-      .then(response => {
-        //console.log(response);
-        // ...
-      }).catch(error => {
-        console.log(error);
-      });     
-      getRooms();       
-  }
 
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -83,15 +19,15 @@ export default function Login() {
         <View style={styles.textInput}>
           <TextInput
             placeholder='E-mail'
-            onChangeText={changeHandlerUsername}
+            onChangeText={setEmail}
             placeholderTextColor='#6272a4'
-            value={username}
+            value={email}
           />
         </View>
         <View style={styles.textInput}>
           <TextInput
             placeholder='Password'
-            onChangeText={changeHandlerPassword}
+            onChangeText={setPassword}
             placeholderTextColor='#6272a4'
             value={password}
             textContentType='password'
@@ -99,28 +35,15 @@ export default function Login() {
           />
         </View>
         <Button
-          onPress={submitHandler}
+          onPress={() => signIn({ email, password })}
           title='Login'
           backgroundColor='#bd93f9'
         />
-
-        <TextInput 
-          styles={styles.textInput}
-          placeholder='Password'
-          onChangeText={changeHandlerPassword}
-          placeholderTextColor='white'
-          value={password}
-          textContentType='password'
-          secureTextEntry={false}
-
-        />
-
-        <Button 
-          onPress={submitHandler} 
-          title='Login'
-          color='purple'
-          />
-
+        <View>
+          <TouchableOpacity>
+          <Text style={styles.text}>Don't have an account? Sign up now!</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </TouchableWithoutFeedback>
   );
